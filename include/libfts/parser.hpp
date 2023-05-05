5d7d4e296c5114bc485b3eeaa5da18e4bb5f2839
Foxisex <smilelikepro@mail.ru>
2023-05-05 20:03:28 +0700

    Add parser

---
+++ include/libfts/parser.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+namespace prsr {
+	struct config {
+		std::vector<std::string> stop_words;
+		int min_ngram_length;
+		int max_ngram_length;
+	};
+
+	struct  ngrams{
+		std::string word;
+		int token_index;
+	};
+
+	bool my_ispunct(char symbol);
+	void str_to_words(std::string &origin, char delim, std::vector<std::string> &result_vec);
+	void rm_stop_words(std::vector<std::string> &result_vec, const std::vector<std::string> &stop_words);
+	void generate_ngrams(const std::vector<std::string> &words, const prsr::config &cfg, std::vector<prsr::ngrams> &parsed_vec);
+	void parser(const std::string &origin, const prsr::config &cfg, std::vector<prsr::ngrams> &parsed_vec);
+} // namespace prsr
--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -1,21 +1,42 @@
 #include <libfts/solver.hpp>
-
+#include <libfts/parser.hpp>
 #include <CLI/CLI.hpp>
 
 #include <iostream>
 
-int main(int argc, char** argv) {
-    CLI::App app{"App description"};
+// int main(int argc, char** argv) {
+//     CLI::App app{"App description"};
 
-    double first_operand = 0;
-    double second_operand = 0;
+//     double first_operand = 0;
+//     double second_operand = 0;
 
-    app.add_option("--first", first_operand, "First");
-    app.add_option("--second", second_operand, "Second");
+//     app.add_option("--first", first_operand, "First");
+//     app.add_option("--second", second_operand, "Second");
 
-    CLI11_PARSE(app, argc, argv);
-    double result = fts::sum(first_operand, second_operand);
-    std::cout << result << std::endl;
+//     CLI11_PARSE(app, argc, argv);
+//     double result = fts::sum(first_operand, second_operand);
+//     std::cout << result << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
+
+int main()
+{
+    std::string orig = "Dr. Jekyll and Mr. Hyde";
+    std::vector<prsr::ngrams> out;
+
+    prsr::config config;
+    config.stop_words = {
+        "a",   "an",    "and",  "are",   "as",    "at",   "be",   "but",
+        "by",  "for",   "if",   "in",    "into",  "is",   "i t",   "no",
+        "not", "of",    "on",   "or",    "s",     "such", "t",    "that",
+        "the", "their", "then", "there", "these", "they", "this", "to",
+        "was", "will",  "with", "dr",    "mr"};
+    config.min_ngram_length = 3;
+    config.max_ngram_length = 6;
+
+    prsr::parser(orig, config, out);
+    for(auto i = out.begin(); i != out.end(); i++)
+        std::cout << i->word << " " << i->token_index << " " ;
+    std::cout << std::endl;
+} 
