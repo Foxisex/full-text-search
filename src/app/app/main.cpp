3ac766f6d16fbad83a96d8b44fc0d6029e35e164
Foxisex <smilelikepro@mail.ru>
2023-05-14 01:08:25 +0700

    Rm parser manual-test from main

--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -4,9 +4,6 @@
 #include <libfts/parser.hpp>
 
 int main() {
-    std::string orig = "Dr. Jekyllll and Mr. the the Hyde";
-    std::vector<prsr::ngrams> out;
-
     prsr::config config;
     config.stop_words = {
         "a",   "an",    "and",  "are",   "as",    "at",   "be",   "but",
@@ -16,12 +13,6 @@ int main() {
         "was", "will",  "with", "dr",    "mr"};
     config.min_ngram_length = 3;
     config.max_ngram_length = 6;
-
-    prsr::parser(orig, config, out);
-    for (auto& ngram : out) {
-        std::cout << ngram.word << " " << ngram.token_index << " ";
-    }
-    std::cout << std::endl;
 
     indexer::IndexBuilder builder(config);
 
